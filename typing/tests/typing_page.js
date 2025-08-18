78487be4af4f119fdec03acef1af5a402f296d06
xyz8853 <ktejas8853@gmail.com>
2025-08-18 12:47:30 +0530

    Update typing_page.js

--- typing/tests/typing_page.js
+++ typing/tests/typing_page.js
@@ -191,4 +191,21 @@ let inputWordData = document.querySelector('#wordTypeInputData');
           /**CPM */
           let cpm = totalChar/totalTypingTime;
           cpmSpeed.textContent=`${cpm}`;
+
       });
+
+    // menu tools name designing
+    let color = ['#C70039','#FFC733','#DAF7A6','#FF5733','#900C3F','#C70039','#6495ED','#CCCCFF','#40E0D0','#00FF00','#0000FF','#FF0000','Lime'];
+    let colorIndex =0;
+    let navContent = document.querySelector('.navbar-heading-text') 
+    let navBarContent = ()=>{
+        
+        navContent.style.color = color[colorIndex];
+        colorIndex++;
+        if(colorIndex>=color.length-1){
+             colorIndex =0;
+        }
+        
+    }
+
+    setInterval(navBarContent,500);
