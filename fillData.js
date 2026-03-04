a10700696c60b17e728d8dabf3643e4ab722a1bb
PRASHANT KUMAR TEJAS <ktejas8853@gmail.com>
2026-03-04 10:39:43 +0530

    Update fillData.js

--- fillData.js
+++ fillData.js
@@ -4,6 +4,7 @@
 // for Acadmic Qualification 
 
 let acadmicIndex = 0;
+let acadmicFlagArr = [];
 let insideAcadmicTd = document.querySelector('.acadmic-all-input');
 let acadmicButton = document.querySelector('.acadmic-add-button');
 
@@ -26,6 +27,7 @@ acadmicButton.addEventListener('click', function(e){
         
         e.preventDefault();
         acadmicIndex++;
+        acadmicFlagArr.push(acadmicIndex);
         if(acadmicIndex<=3){
          let acadmicRemoveButton = document.createElement("button");
             acadmicRemoveButton.setAttribute('class','acadmic-remove-'+acadmicIndex);
@@ -39,30 +41,30 @@ acadmicButton.addEventListener('click', function(e){
             let input1 = document.createElement('input');
             input1.setAttribute('placeholder','Exam');
             input1.setAttribute('type','text');
-            input1.setAttribute('name','aexam'+acadmicIndex);
+            input1.setAttribute('name','clientExam'+acadmicIndex);
             input1.setAttribute('class','input'+acadmicIndex);
             
 
             let input2 = document.createElement('input');
             input2.setAttribute('placeholder','Board/University');
             input2.setAttribute('type','text');
-            input2.setAttribute('name','aboard'+acadmicIndex);
+            input2.setAttribute('name','clientBoardOrUniversity'+acadmicIndex);
 
             let input3 = document.createElement('input');
             input3.setAttribute('placeholder','Passing year');
             input3.setAttribute('type', 'text');
-            input3.setAttribute('name','aPassingYear'+acadmicIndex);
+            input3.setAttribute('name','clientPassingYear'+acadmicIndex);
 
             let input4 = document.createElement('input');
             input4.setAttribute('placeholder', 'Mark %');
             input4.setAttribute('type', 'text');
-            input4.setAttribute('name','aMark'+acadmicIndex);
+            input4.setAttribute('name','clientMarkPercentage'+acadmicIndex);
 
 
             let input5 = document.createElement('input');
             input5.setAttribute('placeholder','Division');
             input5.setAttribute('type','text');
-            input5.setAttribute('name','aDivision'+acadmicIndex);
+            input5.setAttribute('name','clientDivision'+acadmicIndex);
 
             // insideElement.append(input1);
             // insideElement.append(input2);
@@ -101,7 +103,6 @@ acadmicButton.addEventListener('click', function(e){
 
 
 //acadmic remove button concept div first
-
 let parentAcadmic1 = document.querySelector('.acadmic1');
 
     parentAcadmic1.addEventListener('click', function(e){
@@ -141,6 +142,7 @@ let parentAcadmic3 = document.querySelector('.acadmic3');
 // For Prfessional Qualification 
 
     let professionalIndex = 0;    
+    let proFlagArray = [];
     let insideProfessionalTd = document.querySelector('.professional-all-input');
 
     createProfessionalNewDiv = document.createElement('div');
@@ -159,6 +161,7 @@ let parentAcadmic3 = document.querySelector('.acadmic3');
     professionalButton.addEventListener('click', function(e){
         e.preventDefault();
         professionalIndex++;
+        proFlagArray.push(professionalIndex);
         if(professionalIndex<=3){
 
             let professionalRemoveButton = document.createElement('button');
@@ -173,31 +176,31 @@ let parentAcadmic3 = document.querySelector('.acadmic3');
             input1.setAttribute('placeholder','Exam');
             input1.setAttribute('type','text');
             input1.setAttribute('class','input-'+professionalIndex);
-            input1.setAttribute('name','pExam'+professionalIndex);
+            input1.setAttribute('name','clientProExam'+professionalIndex);
 
             let input2 = document.createElement('input');
             input2.setAttribute('placeholder','Board/University');
             input2.setAttribute('type','text');
             input2.setAttribute('class','input-'+professionalIndex);
-            input2.setAttribute('name','pBoard'+professionalIndex);
+            input2.setAttribute('name','clientProBoard'+professionalIndex);
             
             let input3 = document.createElement('input');
             input3.setAttribute('placeholder','Passing year');
             input3.setAttribute('type','text');
             input3.setAttribute('class','input-'+professionalIndex);
-            input3.setAttribute('name','pPassingYear'+professionalIndex);
+            input3.setAttribute('name','clientProPassingYear'+professionalIndex);
 
 
             let input4 = document.createElement('input');
             input4.setAttribute('placeholder','Mark %');
             input4.setAttribute('type','text');
-            input4.setAttribute('name','pMark'+professionalIndex);
+            input4.setAttribute('name','clientProMarkPercentage'+professionalIndex);
 
 
             let input5 = document.createElement('input');
             input5.setAttribute('placeholder','Division');
             input5.setAttribute('type','text');
-            input5.setAttribute('name','pDivision'+professionalIndex);
+            input5.setAttribute('name','clientProDivision'+professionalIndex);
 
             // insideProfessionalElement.append(input1);
             // insideProfessionalElement.append(input2);
@@ -267,6 +270,7 @@ let parentProfessional3 = document.querySelector('.professional-div3');
 
 //Extra Qualification
     let extraIndex = 0;
+    let extraFlagArr = [];
     let extraQualificationButton = document.querySelector('.extra-add-button');
     let insideExtraTd = document.querySelector('.extra-qualification');
 
@@ -278,11 +282,12 @@ let parentProfessional3 = document.querySelector('.professional-div3');
     extraQualificationButton.addEventListener('click', function(e){
         e.preventDefault();
         extraIndex++;
+        extraFlagArr.push(extraIndex);
         if(extraIndex<=1){
            
             let input = document.createElement('input');
             input.setAttribute('type','text');
-            input.setAttribute('name','extraQualification'+extraIndex);
+            input.setAttribute('name','clientExtraQualification'+extraIndex);
 
             let extraRemoveButton = document.createElement('button');
             extraRemoveButton.setAttribute('id', 'extra-remove-button1');
@@ -309,6 +314,7 @@ let parentProfessional3 = document.querySelector('.professional-div3');
 
 // for using work experience
     let workExperienceIndex = 0;
+    let workExpFlagArr = [];
     let workExperienceButton = document.querySelector('#work-new-add-button');
 
     let insideWorkTd = document.querySelector('.work-experience');
@@ -320,6 +326,7 @@ let parentProfessional3 = document.querySelector('.professional-div3');
         workExperienceButton.addEventListener('click', function(e){
             e.preventDefault();
             workExperienceIndex++;
+            workExpFlagArr.push(workExperienceIndex);
             if(workExperienceIndex<=1){
                 
                 let workExperienceRemoveButton = document.createElement('button');
@@ -331,7 +338,7 @@ let parentProfessional3 = document.querySelector('.professional-div3');
 
                 let input = document.createElement('input');
                 input.setAttribute('type','text');
-                input.setAttribute('name','workExperience'+workExperienceIndex);
+                input.setAttribute('name','clientWorkExperience'+workExperienceIndex);
 
                 createWorkNewDiv.append(input);
                 createWorkNewDiv.append(workExperienceRemoveButton);
@@ -367,3 +374,199 @@ let navContent = document.querySelector('.navbar-heading-text')
     }
 
     setInterval(navBarContent,500);
+
+    //captcha changable concept
+    // let signup_button = document.querySelector('.captcha-changble-button');
+    // let inputCaptchaCode = document.querySelector('.input-captch-code');
+    
+    // let code='';
+    // let getInputChaptaCode='';
+    // signup_button.addEventListener('click', function(event){
+    //  event.preventDefault();
+    // code = '';
+    //  for(let r=1; r<=5; r++){
+    //       let captchaCode = 'ABCDEFGHKIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz234567890';
+    //       let char = captchaCode.charAt((Math.floor(Math.random()*captchaCode.length)));
+    //       code = code+char;
+    //  }
+    //  let captchaValue = document.querySelector('.captcha-text');
+    //  captchaValue.setAttribute('value',code);
+    //  console.log(code);
+    // });
+
+    //resume template data handling
+    //template choose option
+    let templateOption = 1;
+    let clientMaxData = [];
+    let acadmicQualification = [];
+    let professionQualification = [];
+    let extraQual = [];
+    let workExp = [];
+    let clientGender='Other';
+    let resumeTemplateFlag=false;
+    let clientInputName ='';
+    function formHandlingData(event){
+        var acadmicObject = {
+        exam:'mca',
+        board:'up',
+        passingYear:'2026',
+        markPercentage:'75',
+        division:'first'
+    }
+        clientMaxData.push(event.target.clientName.value);
+        clientMaxData.push(event.target.clientAddress.value);
+        clientMaxData.push(event.target.clientMobileNumber.value);
+        clientMaxData.push(event.target.clientEmailId.value);
+        clientMaxData.push(event.target.clientDOB.value);
+        clientMaxData.push(event.target.ClientFatherName.value);
+        clientMaxData.push(event.target.clientMotherName.value);
+        clientMaxData.push(event.target.clientNationality.value);
+        clientMaxData.push(event.target.ClientGender.value);
+        clientMaxData.push(event.target.ClientMarital.value);
+        clientMaxData.push(event.target.clientSpeakLanguage.value)
+        clientMaxData.push(event.target.ClientHobbies.value)
+        clientMaxData.push(event.target.clientObjectiveText.value)
+        // clientMaxData.push(event.target.clientPhoto.value);
+        
+        //Qualification
+       acadmicObject.exam = event.target.clientExam.value;
+       acadmicObject.board = event.target.clientBoardOrUniversity.value;
+       acadmicObject.passingYear = event.target.clientPassingYear.value;
+       acadmicObject.markPercentage = event.target.clientMarkPercentage.value;
+       acadmicObject.division = event.target.clientDivision.value;
+       acadmicQualification.push({...acadmicObject});
+       
+       if(acadmicFlagArr[0]==1){
+            acadmicObject.exam = event.target.clientExam1.value;
+            acadmicObject.board = event.target.clientBoardOrUniversity1.value;
+            acadmicObject.passingYear = event.target.clientPassingYear1.value;
+            acadmicObject.markPercentage = event.target.clientMarkPercentage1.value;
+            acadmicObject.division = event.target.clientDivision1.value;
+            acadmicQualification.push({...acadmicObject});
+       }
+       if(acadmicFlagArr[1]==2){
+            acadmicObject.exam = event.target.clientExam2.value;
+            acadmicObject.board = event.target.clientBoardOrUniversity2.value;
+            acadmicObject.passingYear = event.target.clientPassingYear2.value;
+            acadmicObject.markPercentage = event.target.clientMarkPercentage2.value;
+            acadmicObject.division = event.target.clientDivision2.value;
+            acadmicQualification.push({...acadmicObject});
+       }
+       if(acadmicFlagArr[2]==3){
+            acadmicObject.exam = event.target.clientExam3.value;
+            acadmicObject.board = event.target.clientBoardOrUniversity3.value;
+            acadmicObject.passingYear = event.target.clientPassingYear3.value;
+            acadmicObject.markPercentage = event.target.clientMarkPercentage3.value;
+            acadmicObject.division = event.target.clientDivision3.value;
+            acadmicQualification.push({...acadmicObject});
+       }
+
+        //other professional qualification
+        acadmicObject.exam = event.target.clientProExam.value;
+        acadmicObject.board = event.target.clientProBoard.value;
+        acadmicObject.passingYear = event.target.clientProPassingYear.value;
+        acadmicObject.markPercentage = event.target.clientProMarkPercentage.value;
+        acadmicObject.division = event.target.clientProDivision.value;
+        professionQualification.push({...acadmicObject});
+
+        if(proFlagArray[0]==1){
+            acadmicObject.exam = event.target.clientProExam1.value;
+            acadmicObject.board = event.target.clientProBoard1.value;
+            acadmicObject.passingYear = event.target.clientProPassingYear1.value;
+            acadmicObject.markPercentage = event.target.clientProMarkPercentage1.value;
+            acadmicObject.division = event.target.clientProDivision1.value;
+            professionQualification.push({...acadmicObject});
+        }
+        if(proFlagArray[1]==2){
+            acadmicObject.exam = event.target.clientProExam2.value;
+            acadmicObject.board = event.target.clientProBoard2.value;
+            acadmicObject.passingYear = event.target.clientProPassingYear2.value;
+            acadmicObject.markPercentage = event.target.clientProMarkPercentage2.value;
+            acadmicObject.division = event.target.clientProDivision2.value;
+            professionQualification.push({...acadmicObject});
+        }
+        if(proFlagArray[2]==3){
+            acadmicObject.exam = event.target.clientProExam3.value;
+            acadmicObject.board = event.target.clientProBoard3.value;
+            acadmicObject.passingYear = event.target.clientProPassingYear3.value;
+            acadmicObject.markPercentage = event.target.clientProMarkPercentage3.value;
+            acadmicObject.division = event.target.clientProDivision3.value;
+            professionQualification.push({...acadmicObject});
+        }
+        //extra qualification
+        if(event.target.clientExtraQualification.value.length>0){
+            extraQual.push(event.target.clientExtraQualification.value);
+            // console.log(event.target.clientExtraQualification.value)
+        }if(extraFlagArr[0]==1){
+            extraQual.push(event.target.clientExtraQualification1.value);
+            // console.log(event.target.clientExtraQualification1.value)
+        }
+        //work experience
+        if(event.target.clientWorkExperience.value.length>0){
+            workExp.push(event.target.clientWorkExperience.value);
+        }if(workExpFlagArr[0]==1){
+            workExp.push(event.target.clientWorkExperience1.value);
+        }
+
+        //client gender
+        clientGender = event.target.ClientGender.value;
+        resumeTemplateFlag=true;
+        // localStorage
+            localStorage.setItem('name_to_objective', JSON.stringify(clientMaxData));
+            localStorage.setItem('acadmic_qual', JSON.stringify(acadmicQualification));
+            localStorage.setItem('prof_qual', JSON.stringify(professionQualification));
+            localStorage.setItem('extraQual', JSON.stringify(extraQual));
+            localStorage.setItem('workExperience', JSON.stringify(workExp));
+            localStorage.setItem('clientGender', clientGender);
+        //call url 
+        if(templateOption==1){
+            location.assign("template1.html");
+        }else if(templateOption==2){
+            location.assign("template2.html");
+        }else if(templateOption==3){
+            location.assign("template3.html");
+        }else if(templateOption==4){
+            location.assign("templat4.html");
+        }
+
+        
+        
+        event.preventDefault();
+    }
+
+    //image processing work and pass localstorage
+     function storeImageAndRedirect() {
+        const fileInput = document.querySelector('#selectedClientPhoto');
+        const file = fileInput.files[0];
+
+        if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // Store the image data URL in sessionStorage
+            sessionStorage.setItem('uploadedImage', e.target.result);
+            // Redirect to the second page
+            // window.location.href = 'page2.html';
+        };
+        // Read the image file as a Data URL (Base64 string)
+        reader.readAsDataURL(file);
+        } else {
+        alert('Please select an image file.');
+        }
+  }
+
+  let resumeTheme1 = document.querySelector('#resumeTemplate1ActivateButton');
+      resumeTheme1.addEventListener('click', e=>{
+         templateOption=1;
+      })
+  let resumeTheme2 = document.querySelector('#resumeTemplate2ActivateButton');
+      resumeTheme2.addEventListener('click', e=>{
+        templateOption = 2;
+      })
+  let resumeTheme3 = document.querySelector('#resumeTemplate3ActivateButton');
+      resumeTheme3.addEventListener('click', e=>{
+        templateOption = 3;
+      })
+  let resumeTheme4 = document.querySelector('#resumeTemplate4ActivateButton');
+      resumeTheme4.addEventListener('click', e=>{
+        templateOption = 4;
+      })
